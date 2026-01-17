c502678e19e31ef1973ecee8e0f41d7a2fe7b931
parbat-debnath <parbardebnathofficial@gmail.com>
2026-01-17 23:02:30 +0530

    customize popup added

--- modules/dom.js
+++ modules/dom.js
@@ -2,22 +2,21 @@ const root = document.documentElement;
 const themeToggler = document.querySelector("#themeToggler");
 const themeMeta = document.querySelector("#themeMeta");
 
-const navBar = document.querySelector('#navBar');
-const menuBtn = document.querySelector('#menuBtn');
+const navBar = document.querySelector("#navBar");
+const menuBtn = document.querySelector("#menuBtn");
 
 const imageInputBtn = document.querySelector("#imageUploadBtn");
 const uploadedImg = document.querySelector("#uploaded-image");
 
 // menu
 
-menuBtn.addEventListener('click', () => {
-  if(navBar.style.display === 'block') {
-    navBar.style.display = 'none'
+menuBtn.addEventListener("click", () => {
+  if (navBar.style.display === "block") {
+    navBar.style.display = "none";
   } else {
-    navBar.style.display = 'block';
+    navBar.style.display = "block";
   }
 });
-
 
 // theme
 
@@ -37,13 +36,13 @@ themeToggler.addEventListener("click", () => {
   <path d="m6.34 17.66-1.41 1.41" />
   <path d="m19.07 4.93-1.41 1.41" />`;
     console.log("theme toggled");
-    localStorage.setItem('theme', 'dark');
-} else {
+    localStorage.setItem("theme", "dark");
+  } else {
     themeMeta.setAttribute("content", "#ffffff");
     themeToggler.innerHTML = `<path
     d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"
     />`;
-    localStorage.setItem('theme', 'light');
+    localStorage.setItem("theme", "light");
     console.log("theme toggled");
   }
 });
@@ -70,7 +69,6 @@ if (savedTheme) {
   }
 }
 
-
 // image upload
 
 imageInputBtn.addEventListener("click", () => {
@@ -94,3 +92,72 @@ imageInputBtn.addEventListener("click", () => {
   input.click();
 });
 
+// popups
+
+const popupHolder = document.querySelector("#popup-holder");
+const customizeBtn = document.querySelector("#customize-btn");
+customizeBtn.addEventListener("click", () => {
+  popupHolder.style.display = "block";
+
+  const currentAscii = document.querySelector("#asciiHolder img");
+
+  if (!currentAscii) {
+    alert("Please uplad an image first!");
+    return;
+  }
+
+  const asciiClone = currentAscii.cloneNode(true);
+
+  appendFullScreenPopup(createCustimozePopup(asciiClone), popupHolder);
+});
+
+function appendFullScreenPopup(popup, container) {
+  popup.classList.add("fullscreen-popoup");
+  container.appendChild(popup);
+}
+
+function createCustimozePopup(ascii) {
+  const customizePopup = document.createElement("div");
+  customizePopup.classList.add("popup");
+
+  const popupHeader = document.createElement("h1");
+  popupHeader.textContent = "Customize";
+  popupHeader.classList.add("popup-header");
+
+  const asciiHolder = document.createElement("div");
+  asciiHolder.classList.add("ascii-holder");
+  asciiHolder.appendChild(ascii);
+
+  const exSlider = document.createElement("input");
+  exSlider.type = "range";
+  exSlider.id = "exSlider";
+  exSlider.min = "0";
+  exSlider.max = "100";
+  exSlider.classList.add("slider");
+
+  const cancelBtn = document.createElement("button");
+  cancelBtn.textContent = "Cancel";
+  cancelBtn.classList.add("popup-btn");
+  cancelBtn.id = "popup-cancel-btn";
+  cancelBtn.addEventListener("click", () => {
+    customizePopup.parentElement.style.display = "none";
+    customizePopup.parentElement.removeChild(customizePopup);
+  });
+
+  const doneBtn = document.createElement("button");
+  doneBtn.textContent = "done";
+  doneBtn.classList.add("popup-btn");
+  doneBtn.id = "popup-done-btn";
+  doneBtn.addEventListener("click", () => {
+    customizePopup.parentElement.style.display = "none";
+    customizePopup.parentElement.removeChild(customizePopup);
+  });
+
+  customizePopup.appendChild(popupHeader);
+  customizePopup.appendChild(asciiHolder);
+  customizePopup.appendChild(exSlider);
+  customizePopup.appendChild(cancelBtn);
+  customizePopup.appendChild(doneBtn);
+
+  return customizePopup;
+}
